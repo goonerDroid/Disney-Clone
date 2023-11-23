f6bf29bfe366c87d6ad345705cdd7d15fc3f0750
williamj <williamj949@gmail.com>
2023-11-23 00:36:40 -0500

    remove more default styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Hello, lets build a clone for Disney+</h2>
+      <h2>Hello, lets build a clone for Disney+ !!</h2>
     </div>
   );
 }
