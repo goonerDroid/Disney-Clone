c993a3d04a3a207498c6e8d98207137354101cd7
williamj <williamj949@gmail.com>
2023-11-24 19:40:06 -0500

    Add login bg-image, start with CTA container(1/4)

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,15 @@ import styled from "styled-components";
  const Login = (props) => {
     return (
     <Container>
-        <Content>Content</Content>
+        <Content>
+            <CTA>
+                <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+            </CTA>
+            <BgImage/>
+        </Content>
     </Container>
     )
- };
+ }; 
 
  const Container = styled.section`
  overflow: hidden;
@@ -28,6 +33,34 @@ const Content = styled.div`
  flex-direction: column;
  padding: 80px 40px;
  height: 100%;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div`
+  max-width: 650px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
 `;
 
 
